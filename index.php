1b0c453d637c5e04128acbc36e1fa24ce65beb1b
RicardoFederal <rmviaro@gmail.com>
2023-09-17 19:01:12 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SONDAGEM</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+
+<body>
+<p></p>
+<p></p>
+<fieldset style = "width: 50%; margin: 0 auto;">
+    <legend>Sondagem Fácil</legend>
+    <div class="container">
+        <p>Sistema gerador de atividades para sondagem pedagógica</p>
+    </div>
+</fieldset>
+
+<p></p>
+
+<fieldset style = "width: 50%; margin: 0 auto;">
+
+    <div class="container">
+        <form id="principal" action="">
+
+            <button type="button" class="pop" onclick="window.location.href='gerador.php'">Gerar atividade inicial</button>
+            <button type="button" class="pop">Gerar atividade extra</button>
+            <button type="button" class="pop">Gerar mapa de sondagem</button>
+
+        </form>
+    </div>
+</fieldset>
+
+<p></p>
+
+<div id="divFrame"></div>
+
+
+</body>
+
+</html>
